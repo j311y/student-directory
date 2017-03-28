fa5a14e23b827bcb1d42ef55fe89a59552cc8c92
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-03-28 18:20:19 +0100

    added the input_students method that allows user input to student list

--- directory.rb
+++ directory.rb
@@ -18,6 +18,25 @@ def print_header
   puts "--------------"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # whil the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
@@ -28,6 +47,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 # nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
