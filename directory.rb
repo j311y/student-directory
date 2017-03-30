e506fa4fad6bc2b2b8be9c94b1e95a9cd320b04a
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-03-30 11:46:11 +0100

    added a save students function and saving to ignored file

--- directory.rb
+++ directory.rb
@@ -24,6 +24,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -33,6 +34,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -44,6 +47,16 @@ def show_students
   print_header
   print_students(@students)
   print_footer(@students)
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:age], student[:hobbies], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def select_cohort
